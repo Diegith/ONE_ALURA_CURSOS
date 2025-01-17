00dcb38aafffce7f643bc0f054fc1bde650cc5c0
Diegith <diego.albarracinacero@gmail.com>
2025-01-17 03:58:42 -0500

    change the name a function exc1

--- 2.Logica_de_programacion_funciones_datos/excercies/excercies1.js
+++ 2.Logica_de_programacion_funciones_datos/excercies/excercies1.js
@@ -4,6 +4,6 @@ titulo.innerHTML = "Juego del número secreto";
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = "Indica un número del 1 al 10";
 
-function intentoDeUsuario(){
+function verificarIntento(){
     alert('Click desde ek botón');
 }
